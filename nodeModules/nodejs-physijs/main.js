4bf358b1ebf07e29a8db56dadaee8f01d7b26b45
tussiez <49251771+tussiez@users.noreply.github.com>
2021-06-26 10:46:26 -0400

    Remove nodeModules
    -- with .gitignore

--- nodeModules/nodejs-physijs/main.js
+++
@@ -1,7 +0,0 @@
-var NodePhysijs = {
-    THREE: require('./nodejs/libs/three'),
-    Ammo: require('./nodejs/libs/ammo'),
-    Physijs: require('./nodejs/libs/physi')
-};
-
-module.exports = NodePhysijs;
